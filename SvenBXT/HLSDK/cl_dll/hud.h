7b192fc19173f1ea40dea3abb345e899eab654d0
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-02-19 19:13:34 +0500

    Add HL SDK headers

---
+++ SvenBXT/HLSDK/cl_dll/hud.h
@@ -0,0 +1,38 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+//			
+//  hud.h
+//
+// class CHud declaration
+//
+// CHud handles the message, calculation, and drawing the HUD
+//
+
+// YaLTeR - Heavily stripped down to contain only useful stuff.
+
+#define RGB_YELLOWISH 0x00FFA000 //255,160,0
+#define RGB_REDISH 0x00FF1010 //255,160,0
+#define RGB_GREENISH 0x0000A000 //0,160,0
+
+typedef struct {
+	int x, y;
+} POSITION;
+
+typedef struct {
+	unsigned char r, g, b, a;
+} RGBA;
+
+#define HUD_ACTIVE	1
+#define HUD_INTERMISSION 2
