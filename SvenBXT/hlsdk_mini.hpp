21e3a2f2ead8eef6364c2f73be287d30cc3ac30d
ScriptedSnark <sevcenkobogdan855@gmail.com>
2022-05-05 18:57:15 +0500

    Update mini HL SDK header

--- SvenBXT/hlsdk_mini.hpp
+++ SvenBXT/hlsdk_mini.hpp
@@ -451,6 +451,196 @@ struct edict_s
 	// other fields from progs come immediately after
 };
 
+typedef struct
+{
+	std::byte r, g, b;
+} color24;
+
+typedef struct
+{
+	unsigned r, g, b, a;
+} colorVec;
+
+typedef struct entity_state_s entity_state_t;
+
+struct entity_state_s
+{
+	// Fields which are filled in by routines outside of delta compression
+	int			entityType;
+	// Index into cl_entities array for this entity.
+	int			number;
+	float		msg_time;
+
+	// Message number last time the player/entity state was updated.
+	int			messagenum;
+
+	// Fields which can be transitted and reconstructed over the network stream
+	vec3_t		origin;
+	vec3_t		angles;
+
+	int			modelindex;
+	int			sequence;
+	float		frame;
+	int			colormap;
+	short		skin;
+	short		solid;
+	int			effects;
+	float		scale;
+
+	std::byte		eflags;
+
+	// Render information
+	int			rendermode;
+	int			renderamt;
+	color24		rendercolor;
+	int			renderfx;
+
+	int			movetype;
+	float		animtime;
+	float		framerate;
+	int			body;
+	std::byte		controller[4];
+	std::byte		blending[4];
+	vec3_t		velocity;
+
+	// Send bbox down to client for use during prediction.
+	vec3_t		mins;
+	vec3_t		maxs;
+
+	int			aiment;
+	// If owned by a player, the index of that player ( for projectiles ).
+	int			owner;
+
+	// Friction, for prediction.
+	float		friction;
+	// Gravity multiplier
+	float		gravity;
+
+	// PLAYER SPECIFIC
+	int			team;
+	int			playerclass;
+	int			health;
+	qboolean	spectator;
+	int         weaponmodel;
+	int			gaitsequence;
+	// If standing on conveyor, e.g.
+	vec3_t		basevelocity;
+	// Use the crouched hull, or the regular player hull.
+	int			usehull;
+	// Latched buttons last time state updated.
+	int			oldbuttons;
+	// -1 = in air, else pmove entity number
+	int			onground;
+	int			iStepLeft;
+	// How fast we are falling
+	float		flFallVelocity;
+
+	float		fov;
+	int			weaponanim;
+
+	// Parametric movement overrides
+	vec3_t				startpos;
+	vec3_t				endpos;
+	float				impacttime;
+	float				starttime;
+
+	// For mods
+	int			iuser1;
+	int			iuser2;
+	int			iuser3;
+	int			iuser4;
+	float		fuser1;
+	float		fuser2;
+	float		fuser3;
+	float		fuser4;
+	vec3_t		vuser1;
+	vec3_t		vuser2;
+	vec3_t		vuser3;
+	vec3_t		vuser4;
+};
+
+typedef struct efrag_s
+{
+	struct mleaf_s* leaf;
+	struct efrag_s* leafnext;
+	struct cl_entity_s* entity;
+	struct efrag_s* entnext;
+} efrag_t;
+
+typedef struct
+{
+	std::byte					mouthopen;		// 0 = mouth closed, 255 = mouth agape
+	std::byte					sndcount;		// counter for running average
+	int						sndavg;			// running average
+} mouth_t;
+
+typedef struct
+{
+	float					prevanimtime;
+	float					sequencetime;
+	std::byte					prevseqblending[2];
+	vec3_t					prevorigin;
+	vec3_t					prevangles;
+
+	int						prevsequence;
+	float					prevframe;
+
+	std::byte					prevcontroller[4];
+	std::byte					prevblending[2];
+} latchedvars_t;
+
+typedef struct
+{
+	// Time stamp for this movement
+	float					animtime;
+
+	vec3_t					origin;
+	vec3_t					angles;
+} position_history_t;
+
+#define HISTORY_MAX		64  // Must be power of 2
+
+typedef struct cl_entity_s cl_entity_t;
+
+struct cl_entity_s
+{
+	int						index;      // Index into cl_entities ( should match actual slot, but not necessarily )
+
+	qboolean				player;     // True if this entity is a "player"
+
+	entity_state_t			baseline;   // The original state from which to delta during an uncompressed message
+	entity_state_t			prevstate;  // The state information from the penultimate message received from the server
+	entity_state_t			curstate;   // The state information from the last message received from server
+
+	int						current_position;  // Last received history update index
+	position_history_t		ph[HISTORY_MAX];   // History of position and angle updates for this player
+
+	mouth_t					mouth;			// For synchronizing mouth movements.
+
+	latchedvars_t			latched;		// Variables used by studio model rendering routines
+
+	// Information based on interplocation, extrapolation, prediction, or just copied from last msg received.
+	//
+	float					lastmove;
+
+	// Actual render position and angles
+	vec3_t					origin;
+	vec3_t					angles;
+
+	// Attachment points
+	vec3_t					attachment[4];
+
+	// Other entity local information
+	int						trivial_accept;
+
+	struct model_s* model;			// cl.model_precache[ curstate.modelindes ];  all visible entities have a model
+	struct efrag_s* efrag;			// linked list of efrags
+	struct mnode_s* topnode;		// for bmodels, first world node that splits bmodel, or NULL if not split
+
+	float					syncbase;		// for client-side animations -- used by obsolete alias animation system, remove?
+	int						visframe;		// last frame this entity was found in an active leaf
+	colorVec				cvFloorColor;
+};
 typedef struct cl_enginefuncs_s
 {
 	// sprite handlers
